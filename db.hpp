6694d90e48d4fadfbd3d11397571257b52766730
iassasin <iassasin@yandex.ru>
2017-07-19 15:02:43 +0300

    Добавлена обертка для выражений sql с автоматическим освобождением ресурсов

--- db.hpp
+++ db.hpp
@@ -9,10 +9,55 @@
 #include <cppconn/exception.h>
 #include <memory>
 #include <string>
+
 #include "config.hpp"
+#include "algo.hpp"
 
 using std::string;
 using std::unique_ptr;
+
+template <typename T>
+class SafeStatement {
+private:
+	//using T = sql::PreparedStatement;
+	unique_ptr<T> pst;
+public:
+	SafeStatement(SafeStatement<T> &) = delete;
+	SafeStatement(const SafeStatement<T> &) = delete;
+
+	explicit SafeStatement(T *st){
+		pst.reset(st);
+	}
+
+	SafeStatement(SafeStatement &&ss){
+		pst.swap(ss.pst);
+	}
+
+	SafeStatement<T> &operator = (SafeStatement<T> &&ss){
+		pst.swap(ss.pst);
+		return *this;
+	}
+
+	bool execute(){
+		return pst && pst->execute();
+	}
+
+	unique_ptr<sql::ResultSet> executeQuery(){
+		return pst ? as_unique(pst->executeQuery()) : nullptr;
+	}
+
+	int executeUpdate(){
+		return pst ? pst->executeUpdate() : 0;
+	}
+
+	T *operator -> (){
+		return pst.get();
+	}
+
+	T &operator * (){
+		return *pst;
+	}
+};
 
 class Database {
 private:
@@ -21,6 +66,15 @@ private:
 	void init(){
 		execute("set names utf8");
 		execute("use www");
+	}
+
+	bool execute(const string &sql){
+		return statement()->execute(sql);
+	}
+
+	unique_ptr<sql::ResultSet> query(const string &sql){
+		return unique_ptr<sql::ResultSet>(statement()->executeQuery(sql));
+		//TODO: обертка, чтобы сохранять statement()
 	}
 public:
 	Database(){
@@ -36,21 +90,12 @@ public:
 		init();
 	}
 	
-	bool execute(const string &sql){
-		return statement()->execute(sql);
+	SafeStatement<sql::Statement> statement(){
+		return SafeStatement<sql::Statement>(conn->createStatement());
 	}
 	
-	unique_ptr<sql::ResultSet> query(const string &sql){
-		return unique_ptr<sql::ResultSet>(statement()->executeQuery(sql));
-		//TODO: обертка, чтобы сохранять statement()
-	}
-	
-	unique_ptr<sql::Statement> statement(){
-		return unique_ptr<sql::Statement>(conn->createStatement());
-	}
-	
-	unique_ptr<sql::PreparedStatement> prepare(const string &sql){
-		return unique_ptr<sql::PreparedStatement>(conn->prepareStatement(sql));
+	SafeStatement<sql::PreparedStatement> prepare(const string &sql){
+		return SafeStatement<sql::PreparedStatement>(conn->prepareStatement(sql));
 	}
 
 };
